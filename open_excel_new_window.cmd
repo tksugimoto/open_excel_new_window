5c14e1a63c446de0a306f7975e9205ee94bdcf90
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2020-01-17 12:10:39 +0900

    feat: Office16に更新

--- open_excel_new_window.cmd
+++ open_excel_new_window.cmd
@@ -1,11 +1,9 @@
 @ECHO OFF
-SET OFFICE_DIR_32=C:\Program Files (x86)\Microsoft Office\Office14\
-SET OFFICE_DIR_64=C:\Program Files\Microsoft Office\Office14\
 
 SET targetPath=%1
 
 rem PCの環境に合わせて変更
-SET OFFICE_DIR=%OFFICE_DIR_32%
+SET OFFICE_DIR=C:\Program Files\Microsoft Office\root\Office16\
 
 SET EXCEL_EXE_PATH="%OFFICE_DIR%EXCEL.EXE"
 
